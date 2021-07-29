6e7c47cfa0623520b34e10e1759fd11ea2e3abe9
SrishtiToora <77147148+SrishtiToora@users.noreply.github.com>
2021-07-29 22:58:22 +0530

    Time: 228 ms (55.27%), Space: 16 MB (30.55%) - LeetHub

--- next-greater-element-ii/next-greater-element-ii.py
+++ next-greater-element-ii/next-greater-element-ii.py
@@ -1,23 +1,39 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         ans=[]
-        for i in range(len(nums)):
-            j=i+1
-            if i==len(nums)-1:
-                j=0
-            
-            while (1):
-                if j==i:
-                    ans.append(-1)
-                    break
-                if nums[j]>nums[i]:
-                    ans.append(nums[j])
-                    break
-                j+=1
-                if j==len(nums):
-                    j=0
-                    
-        return ans
+        stack=[]
+        x=[i for i in nums]
+        x.pop(-1)
+        x=x[::-1]
+        
+        for i in range(len(x)):
+            if len(stack)==0:
+                stack.append(x[i])
+            else:
+                if stack[-1]<=x[i]:
+                    stack.pop(-1)
+                    stack.append(x[i])
+                elif stack[-1]>x[i]:
+                    stack.append(x[i])
+        
+        for i in reversed(range(len(nums))):
+            if len(stack)==0:
+                ans.append(-1)
+                stack.append(nums[i])
+            else:
+                if stack[-1]>nums[i]:
+                    ans.append(stack[-1])
+                    stack.append(nums[i])
+                else:
+                    while stack and stack[-1]<=nums[i]:
+                        stack.pop(-1)
+                    if len(stack)==0:
+                        ans.append(-1)
+                        stack.append(nums[i])
+                    else:
+                        ans.append(stack[-1])
+                        stack.append(nums[i])
+        return ans[::-1]
                     
             
         
