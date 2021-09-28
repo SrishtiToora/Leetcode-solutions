9fd8f4d34bbcf452e9aca128f481b68503e61de8
SrishtiToora <77147148+SrishtiToora@users.noreply.github.com>
2021-09-28 23:45:59 +0530

    Time: 40 ms (89.75%), Space: 16.3 MB (50.47%) - LeetHub

--- diameter-of-binary-tree/diameter-of-binary-tree.py
+++ diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,14 +6,29 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        self.ans=1
-        def depth(node):
-            if not node:
+        self.res= -101
+        def solve(node,res):
+            
+            if node is None:
                 return 0
-            L=depth(node.left)
-            R=depth(node.right)
-            self.ans=max(self.ans,L+R+1)
-            return max(L,R)+1
-        depth(root)
-        return self.ans-1
+            left=solve(node.left,res)
+            right=solve(node.right,res)
+            
+            temp= max(left,right)+1
+            ans=max(temp, left+right+1)
+            
+            self.res=max(self.res,ans)
+            return temp
+        # self.res= float("-inf")
+        solve(root,self.res)
+        return self.res -1
+            
+            # if not node:
+            #     return 0
+            # L=depth(node.left)
+            # R=depth(node.right)
+            # self.ans=max(self.ans,L+R+1)
+            # return max(L,R)+1
+        # depth(root)
+        # return self.ans-1
         
