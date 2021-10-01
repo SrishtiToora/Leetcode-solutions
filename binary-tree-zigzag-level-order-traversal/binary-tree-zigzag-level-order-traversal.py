d935419c2ac62f8fe0135bc081b4df2470053361
SrishtiToora <77147148+SrishtiToora@users.noreply.github.com>
2021-10-01 13:02:19 +0530

    Time: 86 ms (5.03%), Space: 14.6 MB (10.65%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -4,24 +4,56 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+import queue
 class Solution:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
-        if root is None:
-            return None
         res=[]
-        level=0
-        self.zigzag(root,level,res)
-        return res
-        
-    def zigzag(self,root,level,res):
+        q=queue.Queue()
         if root is None:
             return None
-        if len(res)<level+1:
-            res.append([])
-        if level%2==1:
-            res[level].append(root.val)
-        else:
-            res[level].insert(0,root.val)
-        self.zigzag(root.right,level+1,res)
-        self.zigzag(root.left,level+1,res)
-        print(res)
+        q.put(root)
+        # print(q)
+        i=0
+        while not q.empty():
+            
+            a=[]
+            size=q.qsize()
+            while size!=0:
+                curr=q.get()
+                if i%2==0:
+                    a.append(curr.val)
+                else:
+                    a.insert(0,curr.val)
+                    
+                    
+                
+                if curr.left:
+                    q.put(curr.left)
+                if curr.right:
+                    q.put(curr.right)
+                size-=1
+            if len(a)!=0:
+                res.append(a)
+            i+=1
+        return res 
+        
+        
+#         if root is None:
+#             return None
+#         res=[]
+#         level=0
+#         self.zigzag(root,level,res)
+#         return res
+        
+#     def zigzag(self,root,level,res):
+#         if root is None:
+#             return None
+#         if len(res)<level+1:
+#             res.append([])
+#         if level%2==1:
+#             res[level].append(root.val)
+#         else:
+#             res[level].insert(0,root.val)
+#         self.zigzag(root.right,level+1,res)
+#         self.zigzag(root.left,level+1,res)
+#         #print(res)
