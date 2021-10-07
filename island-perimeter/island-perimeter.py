670f06cc9cd0766c8be53b85e154f07bed4f3325
SrishtiToora <77147148+SrishtiToora@users.noreply.github.com>
2021-10-07 22:19:15 +0530

    Time: 616 ms (59.11%), Space: 14.9 MB (23.55%) - LeetHub

--- island-perimeter/island-perimeter.py
+++ island-perimeter/island-perimeter.py
@@ -2,11 +2,11 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         rows=len(grid)
         cols=len(grid[0])
-        if rows==1 and cols==1:
-            if grid[rows-1][cols-1]==1:
-                return 4
-            else:
-                return 0
+        # if rows==1 and cols==1:
+            # if grid[rows-1][cols-1]==1:
+                # return 4
+            # else:
+                # return 0
         # print(rows,cols)
         startr=-1
         startc=-1
@@ -32,12 +32,24 @@ class Solution:
             # for k in range(4):
             if i<rows-1 and (grid[i+1][j]==1 or grid[i+1][j]==3):
                 part+=1
+                if i<rows-1 and grid[i+1][j]==1:
+                    grid[i+1][j]=3
+                    s.append((i+1,j))
             if i>0 and (grid[i-1][j]==1 or grid[i-1][j]==3 ):
                 part+=1
+                if i>0 and grid[i-1][j]==1:
+                    grid[i-1][j]=3
+                    s.append((i-1,j))
             if j<cols-1 and (grid[i][j+1]==1 or grid[i][j+1]==3 ):
                 part+=1
+                if j<cols-1 and grid[i][j+1]==1:
+                    grid[i][j+1]=3
+                    s.append((i,j+1))
             if  j>0 and (grid[i][j-1]==1 or grid[i][j-1]==3):
                 part+=1
+                if j>0 and grid[i][j-1]==1:
+                    grid[i][j-1]=3
+                    s.append((i,j-1))
             
             # print(x,part)
             if part==0:
@@ -50,18 +62,10 @@ class Solution:
                 ans+=1
             if part==4:
                 ans+=0
-            if j<cols-1 and grid[i][j+1]==1:
-                grid[i][j+1]=3
-                s.append((i,j+1))
-            if j>0 and grid[i][j-1]==1:
-                grid[i][j-1]=3
-                s.append((i,j-1))
-            if i>0 and grid[i-1][j]==1:
-                grid[i-1][j]=3
-                s.append((i-1,j))
-            if i<rows-1 and grid[i+1][j]==1:
-                grid[i+1][j]=3
-                s.append((i+1,j))
+            
+            
+            
+            
             grid[i][j]=3
             # print(grid)
         return ans
